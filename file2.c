4af90a45d90ce335b7fab756a0c84b90366fdece
ishwaryachitsen0523 <ishwaryachitsen98@gmail.com>
2017-06-30 11:00:11 +0530

    Rename file1.c to file2.c

---
+++ file2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the number:");
+scanf("%d",&a);
+if(a>0)
+printf("the number is positive");
+else if(a=0)
+printf("the number is zero");
+else
+printf("the number is negative");
+getch();
+}
